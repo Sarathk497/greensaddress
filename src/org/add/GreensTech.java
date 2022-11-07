05ba3ded54885d5b4ac9a6c02622f28724cb27fe
Sarathk497 <sarathk497@gmail.com>
2022-11-07 22:07:08 +0530

    address of the institute

---
+++ src/org/add/GreensTech.java
@@ -0,0 +1,36 @@
+//Question-2
+
+package org.add;
+
+public class GreensTech {
+
+	private void location1() {
+		System.out.println("Thoraipakkam, Chennai");
+	}
+
+	private void location2() {
+		System.out.println("adayar, Chennnai");
+	}
+
+	private void location3() {
+		System.out.println("Tambaram,Chennai");
+	}
+
+	private void location4() {
+		System.out.println("Velacherry,Chennai");
+	}
+
+	private void location5() {
+		System.out.println("Anna Nagar, Chennai");
+	}
+
+	public static void main(String[] args) {
+		GreensTech g = new GreensTech();
+
+		g.location1();
+		g.location2();
+		g.location3();
+		g.location4();
+		g.location5();
+	}
+}
